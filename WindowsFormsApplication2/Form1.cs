2abf3dd1c18f0417e1a23d654c9cc446102c1a4b
Algorate <algorate@gmail.com>
2018-04-29 23:55:37 +0500

    first commit

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+
+        string userName;
+        public Form1(string un)
+        {
+            userName = un;
+            
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            BReq req = new BReq();
+
+            string review = textBox1.Text.ToString();
+
+
+
+            review.Replace(" ", "%20");
+               
+            
+
+
+
+            label1.Text = req.HttpGet("https://shrouded-everglades-94535.herokuapp.com/functions/addReview-review="+review+",userName="+userName+"");
+
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("1");
+            comboBox1.Items.Add("2");
+            comboBox1.Items.Add("3");
+            comboBox1.Items.Add("4");
+            comboBox1.Items.Add("5");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            BReq req = new BReq();
+
+            string rating = comboBox1.GetItemText(comboBox1.SelectedItem);
+
+
+            req.HttpGet("https://shrouded-everglades-94535.herokuapp.com/functions/setRating-rating="+rating+",usename="+userName+"");
+
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BReq req = new BReq();
+
+            string roomID = textBox2.Text.ToString();
+
+            req.HttpGet("https://shrouded-everglades-94535.herokuapp.com/functions/bookRoom-roomid="+roomID+",uname="+userName+"");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApplication2/RegisterUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class RegisterUser : Form
+    {
+
+        Login loginForm;
+
+        public RegisterUser(Login l)
+        {
+            loginForm = l;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BReq req = new BReq();
+
+
+            string userName = textBox1.Text.ToString();
+            string password = textBox2.Text.ToString();
+            string gender = textBox3.Text.ToString();
+            string age = textBox4.Text.ToString();
+
+
+
+
+            req.HttpGet("https://shrouded-everglades-94535.herokuapp.com/functions/registerUser-uN="+userName+",pass="+password+",gend="+gender+",age="+age+"");
+
+
+            loginForm.Show();
+            this.Close();
+
+
+        }
+    }
+}
